c975c957b4b4458ee791f666b4d633b471574827
Mitchell Michalak <38023623+mmichalak-swe@users.noreply.github.com>
2023-12-14 01:21:44 -0500

    Fix W-L forumla, set top left sheet formatting

--- createWeeklySheet.js
+++ createWeeklySheet.js
@@ -18,14 +18,13 @@ function createWeeklySheet(year, week, mapConfig, mapMembers) {
       sheet = ss.getSheetByName(sheetName);
     }
 
-    const data = ss.getRangeByName('NFL_' + year).getValues(); //Grab again if wasn't populated before
+    const data = ss.getRangeByName('NFL_' + year).getValues(); // Grab again if wasn't populated before
     let maxRows = sheet.getMaxRows();
     let maxCols = sheet.getMaxColumns();
     const numMembers = Number(mapConfig.get('numMembers'));
     const rowCushion = 3;
     const colCushion = 3;
-    let rows = numMembers + rowCushion; // top three rows above member rows, one below
-    let columns;
+    let rows = numMembers + rowCushion; // top three rows above member rows
 
     // Remove extra rows
     if (maxRows < rows){
@@ -56,20 +55,31 @@ function createWeeklySheet(year, week, mapConfig, mapMembers) {
     // Insert members
     sheet.getRange(rowCushion + 1,1,numMembers,1).setValues(arrMemberNames2D);
 
-  // Format member area
-  sheet.getRange(rowCushion + 1,1,numMembers,colCushion).setHorizontalAlignment("center");
-  sheet.getRange(rowCushion + 1,1,numMembers,colCushion).setFontWeight("bold");
-  sheet.getRange(rowCushion + 1,1,numMembers,colCushion).setBorder(null, null, null, null, true, true, null, null);
+    // Format member area
+    sheet.getRange(rowCushion + 1,1,numMembers,colCushion).setHorizontalAlignment("center");
+    sheet.getRange(rowCushion + 1,1,numMembers,colCushion).setFontWeight("bold");
+    sheet.getRange(rowCushion + 1,1,numMembers,colCushion).setBorder(null, null, null, null, true, true, null, null);
   
-  // Setting header values
+    // Setting header values
     sheet.setColumnWidth(1,120);
     sheet.getRange(1,1).setValue('WEEK ' + week);
-    sheet.getRange(1,1,rowCushion - 1,colCushion).merge();
-    sheet.getRange(1,1,rowCushion - 1,colCushion).setBorder(null, null, true, null, null, null, "white", null);
+    sheet.getRange(1,1,rowCushion - 1,1).merge();
+    sheet.getRange(1,1,rowCushion - 1,1).setBorder(true, true, true, true, null, null, "white", null);
     sheet.getRange(1,1,rowCushion,1).setFontWeight("bold");
     sheet.getRange(1,1,rowCushion,1).setFontSize(18);
     sheet.getRange(1,1,rowCushion,1).setVerticalAlignment("middle");
     sheet.getRange(1,1,rowCushion,1).setHorizontalAlignment("center");
+
+    sheet.getRange(1,2,rowCushion - 1,1).merge();
+    sheet.getRange(1,2,rowCushion - 1,1).setBorder(true, true, true, true, null, null, "white", null);
+    sheet.getRange(1,2,rowCushion - 1,1).setFontWeight("bold");
+    sheet.getRange(1,2,rowCushion - 1,1).setFontSize(10);
+    sheet.getRange(1,2,rowCushion - 1,1).setVerticalAlignment("middle");
+    sheet.getRange(1,2,rowCushion - 1,1).setHorizontalAlignment("center");
+    sheet.getRange(1,2,rowCushion - 1,1).insertCheckboxes();
+
+    sheet.getRange(1,3,rowCushion - 1,1).merge();
+    sheet.getRange(1,3,rowCushion - 1,1).setBorder(null, null, true, null, null, null, "white", null);
 
     sheet.getRange(rowCushion,1,1,1).setValue(['MEMBER']);
     sheet.getRange(rowCushion,1,1,1).setFontSize(10);
@@ -141,16 +151,26 @@ function createWeeklySheet(year, week, mapConfig, mapMembers) {
     sheet.deleteColumns(ptrColumn, maxCols-ptrColumn+1);
     maxCols = sheet.getMaxColumns();
 
+    // Set location for checkbox that triggers member points, weekly W-L record forumlas (R1C1 notation)
+    const checkBoxRow = 1;
+    const checkBoxCol = 2;
+    const checkBoxRangeR1C1 = 'R' + checkBoxRow + 'C' + checkBoxCol;
+
+    // Set location for AWAY/HOME range (R1C1 notation)
+    const homeAwayRangeStartR1C1 = 'R' + rowCushion + 'C' + (colCushion + 1);
+    const homeAwayRangeEndR1C1 = 'R' + rowCushion + 'C' + maxCols;
+    const homeAwayRangeR1C1 = homeAwayRangeStartR1C1 + ":" + homeAwayRangeEndR1C1;
+
     // Set formulas for member points
     const pointsStartCol = 2;
-    const pointsEndColOffset = maxCols - 2;
-    sheet.getRange(rowCushion + 1,pointsStartCol,numMembers,1).setFormulaR1C1("=sumCorrectPicks(R[0]C[2]:R[0]C[" + pointsEndColOffset + "], \"#b7e1cd\")")
+    const pointsEndColOffset = maxCols - pointsStartCol;
+    sheet.getRange(rowCushion + 1,pointsStartCol,numMembers,1).setFormulaR1C1("=sumCorrectPicks(R[0]C[2]:R[0]C[" + pointsEndColOffset + "], \"#b7e1cd\", " + checkBoxRangeR1C1 + ")")
 
     // Set formulas for weekly W-L record
     const recordStartCol = 3;
-    const recordEndColOffset = maxCols - 3;
+    const recordEndColOffset = maxCols - recordStartCol;
     sheet.getRange(rowCushion + 1,recordStartCol,numMembers,1)
-    .setFormulaR1C1("=getNumberWeeklyWins(R[0]C[1]:R[0]C[" + recordEndColOffset + "], \"#b7e1cd\") & \"-\" & (COUNTA(R[0]C[1]:R[0]C[" + recordEndColOffset + "])-getNumberWeeklyWins(R[0]C[1]:R[0]C[" + recordEndColOffset + "], \"#b7e1cd\"))");
+    .setFormulaR1C1("=getNumberWeeklyWins(R[0]C[1]:R[0]C[" + recordEndColOffset + "], \"#b7e1cd\", " + checkBoxRangeR1C1 + ") & \"-\" & (ARRAYFORMULA(SUM(COUNTIF(" + homeAwayRangeR1C1 + ",{\"AWAY\",\"HOME\"})))-getNumberWeeklyWins(R[0]C[1]:R[0]C[" + recordEndColOffset + "], \"#b7e1cd\", " + checkBoxRangeR1C1 + "))");
 
     // Set center alignment for member picks
     sheet.getRange(rowCushion + 1,colCushion + 1,numMembers,maxCols-colCushion).setHorizontalAlignment("center");
@@ -196,12 +216,7 @@ function createWeeklySheet(year, week, mapConfig, mapMembers) {
     range.setFontColor('white');
 
     // Sorts descending by column B
-    range.sort({column: 2, ascending: false});
-
-    // (DONE) merge cells below team dropdown, set formula ="IF(R[0]C[-1]=REGEXEXTRACT(R[0]C[-2],"(.*)@.*"),"AWAY","HOME")"
-    // color columns of points green if correct?
-    // (DONE - custom function) SUMIF cells have a certain formatting
-
+    // range.sort({column: 2, ascending: false});
 }
 
   function driverCreateWeeklySheet() {
