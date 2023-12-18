351c45bbc4a05df14a3c9fdd98e34b25652106e0
Mitchell Michalak <38023623+mmichalak-swe@users.noreply.github.com>
2023-12-17 20:50:03 -0500

    scoreBot manual message to group

--- postTextMessageScoreBot.js
+++ postTextMessageScoreBot.js
@@ -3,22 +3,20 @@ function postTextMessageScoreBot(textMessage) {
   
     try {
       var scriptProperties = PropertiesService.getScriptProperties()
-      var botId = scriptProperties.getProperty('botId');
+      var botId = scriptProperties.getProperty('botIdTest');
     } catch (err) {
       Logger.log(`Failed getting property "botId": ${err.message}`);
     }
   
-    // var textMessage = "Testing google apps script w/ properties to post a message!";
-  
-    // TO-DO: Add something similar for top 3 scores
-    // let array = ["Item 1", "Item 2", "Item 3"]
-    // let joinedArray = array.join("\n")
+    const updateHeader = "The top 3 scores entering the 8pm game:\\n";
+    let updateData = [updateHeader, "Jim: 95", "Bret Smith: 94", "Mitchell: 92", "\\nCongrats on Bret to going 12-1 so far this week!"];
+    let joinedUpdateData = updateData.join('\\n');
   
     var url = "https://api.groupme.com/v3/bots/post";
     var headers = {
       "method": "post",
       "payload":'{"bot_id":"' + botId + '",\
-      "text":"' + textMessage +  '"}'
+      "text":"' + joinedUpdateData +  '"}'
     };
   
     var response = UrlFetchApp.fetch(url, headers);
