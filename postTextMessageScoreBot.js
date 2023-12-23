4214bc739c07049fca314afa89ebab0041cc09b2
Mitchell Michalak <38023623+mmichalak-swe@users.noreply.github.com>
2023-12-23 11:09:15 -0500

    add manual standings message to ScoreBot

--- postTextMessageScoreBot.js
+++ postTextMessageScoreBot.js
@@ -3,22 +3,20 @@ function postTextMessageScoreBot(textMessage) {
   
     try {
       var scriptProperties = PropertiesService.getScriptProperties()
-      var botId = scriptProperties.getProperty('botId');
+      var botId = scriptProperties.getProperty('botIdTest');
     } catch (err) {
       Logger.log(`Failed getting property "botId": ${err.message}`);
     }
   
-    // var textMessage = "Testing google apps script w/ properties to post a message!";
-  
-    // TO-DO: Add something similar for top 3 scores
-    // let array = ["Item 1", "Item 2", "Item 3"]
-    // let joinedArray = array.join("\n")
+    const updateHeader = "The top 3 scores for Week 15:\\n";
+    let updateData = [updateHeader, "Bret Smith: 103", "Phil Jones: 101", "Jim: 99", "\\nCongrats Bret on the week 15 win!"];
+    let joinedUpdateData = updateData.join('\\n');
   
     var url = "https://api.groupme.com/v3/bots/post";
     var headers = {
       "method": "post",
       "payload":'{"bot_id":"' + botId + '",\
-      "text":"' + textMessage +  '"}'
+      "text":"' + joinedUpdateData +  '"}'
     };
   
     var response = UrlFetchApp.fetch(url, headers);
